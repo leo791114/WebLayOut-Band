5ff711d158d031725f0f0d0df8dadd3ed78ce0a8
leo791114 <leo791114@hotmail.com>
2017-04-17 20:06:04 +0800

    finish Band example

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 $(function () {
     myMap();
+    initToolTip();
+    smoothScrolling();
+    slideEffect();
 });
 
 function myMap() {
@@ -11,9 +14,44 @@ function myMap() {
         scrollwheel: false,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = google.maps.Map(document.getElementById('googleMap'), mapProp);
-    var marker = google.maps.Marker({
+    var map = new google.maps.Map($("#googleMap")[0], mapProp);
+    var marker = new google.maps.Marker({
         position: myCenter
     });
     marker.setMap(map);
+}
+
+function initToolTip() {
+    $('[data-toggle = "tooltip"]').tooltip();
+}
+
+function smoothScrolling() {
+    $(".navbar a, footer a[href = '#myPage']").on('click', function (event) {
+        // make sure this.hash has a value before overriding default behavior
+        if (this.hash !== '') {
+            // prevent default anchor click behavior
+            event.preventDefault();
+            // store hash
+            var hash = this.hash;
+            // use jquery animate() method to add smooth page scroll
+            // the optional number (900) specifies the number of milliseconds it takes to scroll to the specific area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+}
+
+function slideEffect() {
+    $(window).scroll(function () {
+        $('.slideadmin').each(function () {
+            var position = $(this).offset().top;
+            var winTop = $(window).scrollTop();
+            if (position < winTop + 600) {
+                $(this).addClass('slide');
+            }
+        });
+    });
 }
